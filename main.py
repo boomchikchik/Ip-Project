4d7620d7e855873ce9f090320618c69e7df66104
boomchikchik <bulkdeals99@gmail.com>
2025-08-07 22:47:23 +0530

    trial

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 #FIRST STEP OF CODE WILL BE WRITTEN HERE
 #This will be main file need to be Executed 
-
+import os
+import sys
 
 def main_func(args,kwargs):
-  pass
+    pass
+  
+
+def relaunch_in_cmd():
+    if os.name == 'nt':
+        if not sys.stdin.isatty():
+            script = os.path.abspath(sys.argv[0])
+            os.system(f'start cmd /k python "{script}"')
+            sys.exit()
+
+
+
+# Your actual program logic below
+print("This is your CLI app. It's running in a real terminal now.")
+input("Press Enter to exit...")
+
 
 
 
 if __name__ == '__main__':
+  relaunch_in_cmd()
   pass #running main function 
   
